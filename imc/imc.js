4260c531991e86a15161140a19ed5b2ea7ae871d
Daniel Da Silva <100931944+projdanielsilva@users.noreply.github.com>
2023-09-11 18:12:25 -0300

    IMC

---
+++ imc/imc.js
@@ -0,0 +1,22 @@
+function calcula_imc(){
+    var altura = document.imcForm.altura.value;
+    var peso = document.imcForm.value;
+
+    var calculo = (peso/(altura*altura));
+
+    if(calculo<18.5){
+        alert("Você está magro com o índice: " + calculo);   
+    }
+    else if(calculo>=18.5 && calculo<24.9){
+        alert("Você está normal com esse índice: " + calculo);
+    }
+    else if(calculo>=25 && calculo<29.9){
+        alert("Você está normal com esse índice: " + calculo);
+    }
+    else if(calculo>=30 && calculo<34.9){
+        alert("Você está normal com esse índice: " + calculo);
+    }
+    else if(calculo>40){
+        alert("Você está normal com esse índice: " + calculo);
+    }
+}
